147776d861f6997287367c508960867087db2a9d
Vlad Tkachenko <vladtka@ukr.net>
2020-10-12 12:32:21 +0300

    добавил prompt и confirm для переменных. Создал переменную budgetMonth для подсчета бюджета на месяц. Исходя из этого пересчитал бюджет на день. Написал конструкцию условий для вывода уровня дохода

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let money = 10000;
 let income = 'фриланс, реклама';
 let addExpenses = 'Коммуналка, интернет, еда, транспорт';
@@ -18,4 +20,31 @@ console.log(addExpenses.toLowerCase());
 console.log(addExpenses.split(', '));
 
 console.log(budgetDay);
+
+money = +prompt('Ваш месячный доход?');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+deposit = confirm('Есть ли у вас депозит в банке?');
+
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let amount1 = +prompt('Во сколько это обойдется?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
+let amount2 = +prompt('Во сколько это обойдется?');
+let budgetMonth = money - (amount1 + amount2);
+
+console.log('Бюджет на месяц: ', budgetMonth);
+console.log('Цель будет достигнута за: ', Math.ceil(mission / budgetMonth), ' месяцев');
+
+budgetDay = budgetMonth / 30;
+
+console.log('Бюджет на день: ', Math.floor(budgetDay));
+
+if (budgetDay >= 1200) {
+  console.log('У вас высокий уровень дохода');
+} else if (budgetDay < 1200 && budgetDay >= 600 ) {
+  console.log('У вас средний уровень дохода');
+} else if (budgetDay < 600 && budgetDay >= 0 ) {
+  console.log('К сожалению у вас уровень дохода ниже среднего');
+} else {
+  console.log('Что то пошло не так');
+}
 
