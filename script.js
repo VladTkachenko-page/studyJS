23508ee3c689bccbc3ffc41de54dc3ea2bb59c7a
Vlad Tkachenko <vladtka@ukr.net>
2020-10-14 13:16:49 +0300

    создал переменную lorem  с текстом и сделал функцию cutText  для проверки на строку, убирания пробелов в начале и конце и скрытия текста

--- script.js
+++ script.js
@@ -1,67 +1,19 @@
 'use strict';
 
-let money = 10000;
-let income = 'фриланс, реклама';
-let addExpenses = 'Коммуналка, интернет, еда, транспорт';
-let deposit = true;
-let mission = 500000;
-let period = 6;
-let budgetDay = money / 30;
+let lorem = 'Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.';
 
-function showTypeOf (data) {
-  console.log(typeof(data));
-}
+function cutText(text) {
+  if (typeof text !== 'string') {
+    alert('Передана не строка');
+  };
+  
+  text = text.trim();
 
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-
-console.log(addExpenses.split(', '));
-
-money = +prompt('Ваш месячный доход?');
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-deposit = confirm('Есть ли у вас депозит в банке?');
-
-let expenses1 = prompt('Введите обязательную статью расходов?');
-let amount1 = +prompt('Во сколько это обойдется?');
-let expenses2 = prompt('Введите обязательную статью расходов?');
-let amount2 = +prompt('Во сколько это обойдется?');
-
-// console.log('Бюджет на месяц: ', budgetMonth);
-// console.log('Цель будет достигнута за: ', Math.ceil(mission / budgetMonth), ' месяцев');
-function getExpensesMonth () {
-  return amount1 + amount2;
+  if (text.length >= 30) {
+    return text.slice(0, 30) + '...';
+  } else {
+    return text;
+  }
 };
 
-console.log('Расходы за месяц: ',getExpensesMonth (amount1, amount2));
-
-function getAccumulatedMonth() {
-  return money - getExpensesMonth (amount1, amount2);
-}
-
-const accumulatedMonth = getAccumulatedMonth();
-
-function getTargetMonth() {
-  return Math.ceil(mission / accumulatedMonth);
-}
-
-console.log('Цель будет достигнута за: ', getTargetMonth(), ' месяцев');
-
-budgetDay = accumulatedMonth / 30;
-
-console.log('Бюджет на день: ', Math.floor(budgetDay));
-
-function getStatusIncome() {
-  if (budgetDay >= 1200) {
-    return('У вас высокий уровень дохода');
-  } else if (budgetDay < 1200 && budgetDay >= 600 ) {
-    return('У вас средний уровень дохода');
-  } else if (budgetDay < 600 && budgetDay >= 0 ) {
-    return('К сожалению у вас уровень дохода ниже среднего');
-  } else {
-    return('Что то пошло не так');
-  }
-}
-
-console.log(getStatusIncome());
-
+console.log(cutText(lorem));
