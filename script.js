fca2a430969a349871941e21c824e8f0edd9f29b
Vlad Tkachenko <vladtka@ukr.net>
2020-10-16 15:48:14 +0300

    создал масив с числами и сделал перебор массива для нахождения числа начинающегося с 2 или 4. Создал цикл для нахождения простых чисел

--- script.js
+++ script.js
@@ -1,92 +1,20 @@
 'use strict';
 
-let isNumber = function(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
+let arr = ['24658', '584946', '489466', '684645', '589846', '486315', '24846'];
 
-let money;
-let income = 'фриланс, реклама';
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-let deposit = confirm('Есть ли у вас депозит в банке?');
-let mission = 500000;
-let period = 6;
-let budgetDay = money / 30;
-
-let start = function() {
-  do {
-    money = prompt('Ваш месячный доход?');
-  } 
-  while (!isNumber(money));
-};
-
-start();
-
-function showTypeOf (data) {
-  console.log(typeof(data));
-}
-
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-
-console.log(addExpenses.split(', '));
-
-let expenses = [];
-
-let getExpensesMonth = function() {
-  let sum = 0;
-  
-  for ( let i = 0; i < 2; i++) {
-
-    expenses[i] =  prompt('Введите обязательную статью расходов?');
-
-    let amount;
-
-    do {
-      amount = prompt('Во сколько это обойдется?');
-    } while (!isNumber(amount));
-    
-    sum += +amount;
-  }
-  
-  return sum;
-}
-
-let expensesAmount = getExpensesMonth();
-
-console.log('Расходы за месяц: ', expensesAmount);
-
-function getAccumulatedMonth() {
-  return money - expensesAmount;
-}
-
-const accumulatedMonth = getAccumulatedMonth();
-
-function getTargetMonth() {
-  return Math.ceil(mission / accumulatedMonth);
-}
-
-if (getTargetMonth() < 0) {
-  console.log('Цель не будет достигнута');
-} else {
-  console.log('Цель будет достигнута за: ', getTargetMonth(), ' месяцев');
-}
-
-budgetDay = accumulatedMonth / 30;
-
-console.log('Бюджет на день: ', Math.floor(budgetDay));
-
-function getStatusIncome() {
-  if (budgetDay >= 1200) {
-    return('У вас высокий уровень дохода');
-  } else if (budgetDay < 1200 && budgetDay >= 600 ) {
-    return('У вас средний уровень дохода');
-  } else if (budgetDay < 600 && budgetDay >= 0 ) {
-    return('К сожалению у вас уровень дохода ниже среднего');
-  } else {
-    return('Что то пошло не так');
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i].substr(0, 1) === '2' || arr[i].substr(0, 1) === '4') {
+    console.log('arr[i]: ', arr[i]);
   }
 }
 
-console.log(getStatusIncome());
-
+checkj:
+for (let i = 2; i >= 1 && i < 100; i++) {
+  let j;
+  for (j = 2; j < i; j++ ) {
+    if ( i % j === 0 ) {
+      continue checkj;
+    }
+  }     
+  console.log('i: ', i, 'Делители этого числа: 1 и', j);
+}
