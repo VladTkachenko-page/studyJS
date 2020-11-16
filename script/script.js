287c00acd38d8d8aba1b63f17fa0db511a9863c6
Vlad Tkachenko <vladtka@ukr.net>
2020-11-16 15:51:50 +0200

    feat (script.js): new feat

--- script/script.js
+++ script/script.js
@@ -1,50 +1,25 @@
-
-window.addEventListener('DOMContentLoaded', function(){
-  'use strict';
+'use strict';
   
-  function countTimer(deadline){
-    let timerHours = document.querySelector('#timer-hours'),
-        timerMinutes = document.querySelector('#timer-minutes'),
-        timerSeconds = document.querySelector('#timer-seconds');
+  let input = document.querySelector('input'),
+        text = document.querySelector('.text');
 
-  function getTimeRemaining(){
-    let dateStop = new Date(deadline).getTime(),
-        dateNow = new Date().getTime(),
-        timeRemaining = (dateStop - dateNow) / 1000,
-        seconds = Math.floor(timeRemaining % 60),
-        minutes = Math.floor((timeRemaining / 60) % 60),
-        hours = Math.floor(timeRemaining / 60 / 60) % 24;
-        return { timeRemaining, hours, minutes, seconds };
-  };
+        function debounce(func, wait, immediate) {
+          var timeout;
+          return function() {
+            var context = this, args = arguments;
+            var later = function() {
+              timeout = null;
+              if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+          };
+        };
+        
+        function addText() {
+          text.textContent = input.value;
+        };
 
-  function addZero(numberTime) {
-    if (numberTime < 10) {
-      return '0' + numberTime;
-    } else {
-      return numberTime;
-    }
-  };
-
-  function updateClock() {
-    let timer =  getTimeRemaining();
-    let timerId = setInterval(updateClock, 1000);
-    timerHours.textContent = addZero(timer.hours);
-    timerMinutes.textContent = addZero(timer.minutes);
-    timerSeconds.textContent = addZero(timer.seconds);
-    if (timer.timeRemaining > 0) {
-      timerId; 
-    } else if (timer.timeRemaining < 0){
-      timerHours.textContent = '00';
-      timerMinutes.textContent = '00';
-      timerSeconds.textContent = '00';
-      clearInterval(timerId);
-    }
-  };
-
-  updateClock();
-    
-  }
-
-  countTimer('01 december 2020');
-
-});
+        input.addEventListener('input', debounce(addText, 300));
